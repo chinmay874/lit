af108dbbfb73a2d34f27f5b7703d1c6ebf4696f6
Chinmay jain <chinmay874@gmail.com>
2024-01-03 11:51:16 +0530

    adding services and env file

--- src/SideBar/SideBar.jsx
+++ src/SideBar/SideBar.jsx
@@ -4,10 +4,11 @@ export default function SideBar(){
     return(
         <div className="sidebar">
             <ul>
-                <li className="list_font">Home</li>
                 <li className="list_font">General</li>
+                <li className="list_font">Inbox</li>
+                <li className="list_font">TimeSheet</li>
                 <li className="list_font">Company Profile</li>
-                <li className="list_font">Assets</li>
+                <li className="list_font">Apps</li>
                 <li className="list_font">Help</li>
             </ul>
 
